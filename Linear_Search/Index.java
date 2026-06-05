568dca7f110a2306eed0408530040eac77f269f6
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-05 21:10:15 +0530

    linear search 1.1

--- Linear_Search/Index.java
+++ Linear_Search/Index.java
@@ -1,25 +1,48 @@
 public class Index {
-    
-    public static int linear_search(int nums[] , int target) {
 
+    // Method to perform Linear Search
+    // Returns the index of the target element if found
+    // Returns -1 if the target is not present in the array
+    public static int linear_search(int nums[], int target) {
+
+        // Get the size of the array
         int n = nums.length;
 
-        for(int i=0; i<n; i++) {
+        // Traverse the array element by element
+        for (int i = 0; i < n; i++) {
 
-            if(nums[i] == target) return i;
+            // Check if the current element matches the target
+            if (nums[i] == target) {
+
+                // Target found, return its index
+                return i;
+            }
         }
+
+        // Target not found after checking all elements
         return -1;
     }
 
     public static void main(String[] args) {
-        int [] nums = {10,0,-1,5,-7,33,9};
 
-        int target = -77;
-        int result = linear_search(nums,target);
+        // Input array
+        int[] nums = {10, 0, -1, 5, -7, 33, 9};
 
-        if(result != -1) {
-            System.out.println("Target found at index: "+result);
-        }else {
+        // Element to search for
+        int target = -7;
+
+        // Call the linear search method
+        int result = linear_search(nums, target);
+
+        // Check the returned value
+        if (result != -1) {
+
+            // Target found
+            System.out.println("Target found at index: " + result);
+
+        } else {
+
+            // Target not found
             System.out.println("Target not found..?");
         }
     }
